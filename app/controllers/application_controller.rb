9b27df3ee00bf21cf30bed35d08c48bf552797f6
Eina Onting <einers@outlook.com>
2020-03-03 23:15:55 +0000

    conditionally show cart table + stripe button based on cart item length

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
-  helper_method :enhanced_cart
+  helper_method :enhanced_cart      
 
   def cart_subtotal_cents
     enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
