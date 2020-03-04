4c197f4fdd9d7f21bdd01c2e777e18857596528d
Eina Onting <einers@outlook.com>
2020-03-04 21:23:01 +0000

    create user methods + add has_active_password to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  def new    
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.user_id
+      redirect_to '/'
+    else 
+      redirect_to '/signup'
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
