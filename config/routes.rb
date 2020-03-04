4e89b5f69947c4f4e931c6b547c7440ba4614337
Eina Onting <einers@outlook.com>
2020-03-04 18:45:14 +0000

    generate about controller and add to rotue

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
 
-  root to: 'products#index'
+  root to: 'products#index'  
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:show]  
 
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: [:create, :show]  
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-  end
+  end  
+
+  get 'about' => 'about#show'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
