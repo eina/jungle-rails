422afb2c460ce97db62ea0ee8457b0bc51ffae8f
Eina Onting <einers@outlook.com>
2020-03-04 21:04:19 +0000

    create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#show'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
