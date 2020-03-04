f6a713adfae37ac024af41cd84787050da9e038a
Eina Onting <einers@outlook.com>
2020-03-04 01:03:09 +0000

    implement order details page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find(params[:id])         
+    # raise @order.inspect 
   end
 
   def create
