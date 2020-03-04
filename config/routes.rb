082df4d1b942a086287910128463f2863082c8a8
Eina Onting <einers@outlook.com>
2020-03-04 21:31:28 +0000

    create log in form + methods

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#show'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
