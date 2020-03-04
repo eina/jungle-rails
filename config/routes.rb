0928753aa62e1040d8e9d0a0f2500077aeea6b2b
Eina Onting <einers@outlook.com>
2020-03-04 22:30:51 +0000

    add sales route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
+    resources :sales, only: [:index]
   end  
 
   get 'about' => 'about#show'
