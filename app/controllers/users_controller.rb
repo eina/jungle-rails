d470be3cfbcb4b2aad87085b705045ffb7a08929
Eina Onting <einers@outlook.com>
2020-03-04 21:19:00 +0000

    create sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
   end
-      
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'  
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]  
 
