f75a5873edc81121f543e01fa8a91012a5ed9b7e
Eina Onting <einers@outlook.com>
2020-03-04 21:45:42 +0000

    implement login form + add indicators to DOM

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  def new    
+  def new        
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to '/'
     else 
       redirect_to '/signup'
