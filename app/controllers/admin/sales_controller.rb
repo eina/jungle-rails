868d42d852d072eed2d0b7566f09d1bd25b4b470
Eina Onting <einers@outlook.com>
2020-03-04 23:07:19 +0000

    add sales model + render sales page

---
+++ app/controllers/admin/sales_controller.rb
@@ -0,0 +1,8 @@
+class Admin::SalesController < ApplicationController
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
-    resources :sales, only: [:index]
+    resources :sales, only: [:index, :new]
   end  
 
   get 'about' => 'about#show'
