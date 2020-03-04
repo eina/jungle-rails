b93ae4459f2bb3164be2405d6ae0566a2d8e9232
Eina Onting <einers@outlook.com>
2020-03-04 18:32:19 +0000

    create admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PW"]
 
   def show
+    @products_total = Product.count
+    @categories_total = Category.count
+
+    # raise @categories_total.inspect
   end
 end
